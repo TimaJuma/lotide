f8fed21d09fa1083cf8c539bc6ed7148979af230
TimaJuma <tima.xpl@gmail.com>
2020-07-21 01:51:36 +0000

    assertion function with emojis

---
+++ assertEqual.js
@@ -0,0 +1,16 @@
+// emoji variable to output with assertion
+let emoji = require('node-emoji');
+
+
+const assertEqual = function(actual, expected) {
+  // check if 2 passsed arguments are equal
+  if (actual === expected) {
+    console.log(emoji.get('heart'), ` Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(emoji.get('warning'), ` Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+// test function
+assertEqual('Lighthouse Labs', "Bootcamp");
+assertEqual(1, 1);
